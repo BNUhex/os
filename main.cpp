367811f2f360c393c6d1421daf094b5be3cc6d3e
santonin386 <130654372+santonin386@users.noreply.github.com>
2024-12-24 16:27:08 +0800

    function03_01 & function03_02 & function03_03

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 struct hinode 	hinode[NHINO];
 
+// function03_01：在命令提示符前加入路径
+char* address[200]; // 路径
+int p_address;      // 索引指针
+
 struct dir 		dir;
 struct file 	sys_ofile[SYSOPENFILE];
 struct filsys 	filsys;
@@ -19,6 +23,12 @@ int main(){
 	int username;
 	char password[16];
 	user_id = -1;
+
+    // function03_02：在命令提示符前加入路径，初始化路径和索引指针
+    p_address = 0;
+    for (int i = 0; i < 100; i++) {
+        address[i] = (char*)malloc(sizeof(char) * 100);
+    }
 
 	// function01_02：改用文件模拟磁盘。判断当前目录下是否存在‘disk.txt’文件
 	FILE* fp = fopen("disk.txt", "rb");
@@ -49,6 +59,13 @@ int main(){
 		user_id = login(username,password);
 	}
 	do{
+        // function03_03：在命令提示符前加入路径
+        if (p_address == 0) {
+            printf("/");
+        }
+        for (int i = 0; i < p_address; i++) {
+            printf("/%s", address[i]);
+        }
 		printf("> ");
 		fflush(stdin);
 		gets(str);
