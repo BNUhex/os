f35aba60c8cbb4dbf8ef0a11121705473ce2c2e2
santonin386 <130654372+santonin386@users.noreply.github.com>
2024-12-24 08:43:51 +0800

    function01_01
    使用文件模拟磁盘

--- halt.cpp
+++ halt.cpp
@@ -1,61 +1,38 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
 #include "filesys.h"
 
+// 停止文件系统的运行，清理资源并安全退出
 void halt(){
 	int i,j;
 
-	/*1. write back the current dir */ 
-	chdir("..");
-	iput(cur_path_inode);
+	/*1. 写回当前目录的内容 */
+	chdir("..");            // 切换到上一级目录
+	iput(cur_path_inode);   // 将当前目录的 i 节点写回磁盘
 
-	/*2. free the u_ofile and sys_ofile and inode*/ 
+	/*2. 释放用户打开的文件表和系统打开文件表，并释放 i 节点*/
 	for(i=0; i<USERNUM; i++){
-		if(user[i].u_uid != 0){
+		if(user[i].u_uid != 0){ // 检查用户是否存在（UID不为0）
 			for(j=0; j<NOFILE;j++){
-				if(user[i].u_ofile[j] != SYSOPENFILE + 1){
-					close(i,j);
-					user[i].u_ofile[j] = SYSOPENFILE + 1;
+				if(user[i].u_ofile[j] != SYSOPENFILE + 1){  // 检查文件是否已关闭
+					close(i,j);                 // 关闭用户打开的文件
+					user[i].u_ofile[j] = SYSOPENFILE + 1;   // 将文件描述符置为无效
 				}//if
 			}//for
 		}//if
 	}//for
 
-	/*3. write back the filesys to the disk*/
+	/*3. 将超级块内容写回磁盘 */
 	memcpy(disk+BLOCKSIZ, &filsys, sizeof(struct filsys));
 
-	/*4. close the file system column*/
+    // function01_01：改用文件模拟磁盘
+    FILE* fp = fopen("disk.txt", "wb"); // 打开文件
+    fwrite(disk, sizeof(char), (DINODEBLK + FILEBLK + 2) * BLOCKSIZ, fp);
+
+	/*4. 关闭文件系统 */
+    fclose(fp);
 
 	/*5. say GOOD BYE to all the user*/
 	printf("\nGood Bye. See You Next Time. Please turn off the switch\n");
 	exit(0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
