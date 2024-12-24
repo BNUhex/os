a0fb17d3d091ebc6cff2b551c20893a48dec5f4b
santonin386 <130654372+santonin386@users.noreply.github.com>
2024-12-24 08:48:06 +0800

    function01_03
    使用文件模拟磁盘

--- install.cpp
+++ install.cpp
@@ -3,27 +3,27 @@
 #include "filesys.h"
 
 /*
-    ��װ�ļ�ϵͳ�����ļ�ϵͳ�ĸ������ݴӴ���(disk����)�����ڴ�
+    安装文件系统，将文件系统的各种数据从磁盘(disk变量)读入内存
 */
 
 void install(){
 	int i,j;
 
-	/* 1. �ӳ������ж�ȡ�ļ�ϵͳ */   //xiao
+	/* 1. 从超级块中读取文件系统 */   //xiao
 	memcpy(&filsys,disk+BLOCKSIZ,sizeof(struct filsys));
 
-	/* 2. ��ʼ���ڴ�i�ڵ��ϣ���� */
+	/* 2. 初始化内存i节点哈希链表 */
 	for (i=0; i<NHINO; i++){
 		hinode[i].i_forw = NULL;
 	}
 
-	/* 3. ��ʼ��ϵͳ���ļ��� */
+	/* 3. 初始化系统打开文件表 */
 	for (i=0; i<SYSOPENFILE; i++){
 		sys_ofile[i].f_count = 0;
 		sys_ofile[i].f_inode = NULL;
 	}
 
-	/* 4. ��ʼ���û� */
+	/* 4. 初始化用户 */
 	for (i=0; i<USERNUM; i++){
 		user[i].u_uid = 0;
 		user[i].u_gid = 0;
@@ -31,25 +31,31 @@ void install(){
 			user[i].u_ofile[j] = SYSOPENFILE+1;
 	}
 
-	/* 5. ��ȡ��Ŀ¼����ʼ��dir */
+	/* 5. 读取主目录来初始化dir */
 	cur_path_inode = iget(1);
-	strcpy(cur_path_name, "/");  // bug04_fix_03����ǰĿ¼��
+	strcpy(cur_path_name, "/");  // bug04_fix_03：当前目录名
 	dir.size = cur_path_inode->di_size/(DIRSIZ+4);//xiao 2-->4
 
-    // bug: ��ʼ���ַ���Ϊ14���ո񣬵�d_name��СΪ12����˿��ܵ������
-    // fix: ���ַ�����ʼ��Ϊ11���ո�
+    // bug: 初始化字符串为14个空格，但d_name大小为12，因此可能导致溢出
+    // fix: 将字符串初始化为11个空格
 	for (i=0; i<DIRNUM; i++){
 		strcpy(dir.direct[i].d_name,"           ");
 		dir.direct[i].d_ino = 0;
 	}
 
+    // 将当前目录的内容读入内存
+    // dir.size / (BLOCKSIZ / (DIRSIZ + 4)) 表示存放当前目录内容的数据块数
 	for (i=0; i<dir.size/(BLOCKSIZ/(DIRSIZ+4)); i++){
 		memcpy(&dir.direct[(BLOCKSIZ/(DIRSIZ+4))*i],
 		disk+DATASTART+BLOCKSIZ*cur_path_inode->di_addr[i], DINODESIZ);
 	}
 
+    // bug：源代码使用固定的 DINODESIZ 进行拷贝，若最后一块存储的数据不足一整
+    //      块，会导致多拷贝无效数据
+    // fix：将 DINODESIZ 改为 (dir.size * (DIRSIZ + 4)) % BLOCKSIZ
 	memcpy(&dir.direct[(BLOCKSIZ)/(DIRSIZ+4)*i],
-	disk+DATASTART+BLOCKSIZ*cur_path_inode->di_addr[i], DINODESIZ);
+	disk+DATASTART+BLOCKSIZ*cur_path_inode->di_addr[i],
+	(dir.size * (DIRSIZ + 4)) % BLOCKSIZ);
 	return;
 }
 
